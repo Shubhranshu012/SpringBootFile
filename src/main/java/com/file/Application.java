098229cc548a9e53f3e7bcf84221c386257d9fdd
Shubhranshu Satpathy <119298751+Shubhranshu012@users.noreply.github.com>
2025-12-11 17:02:51 +0530

    Adding all the files

---
+++ src/main/java/com/file/Application.java
@@ -0,0 +1,24 @@
+package com.file;
+
+import jakarta.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.file.service.FilesStorageService;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner  {
+	@Resource
+	FilesStorageService storageService;
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	public void run(String... arg) throws Exception {
+	    storageService.init();
+	 }
+
+}
